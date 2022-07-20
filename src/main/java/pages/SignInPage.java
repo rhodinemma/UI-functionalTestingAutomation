bb443f16cc95d1c6de6802c247e24280fb31313c
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-07-20 11:39:06 +0300

    add website pages part1

---
+++ src/main/java/pages/SignInPage.java
@@ -0,0 +1,42 @@
+package pages;
+
+import drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignInPage {
+    private WebDriver driver;
+
+    public SignInPage(){
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "email")
+    private WebElement emailField;
+
+    @FindBy(id = "passwd")
+    private WebElement password;
+
+    @FindBy(id = "SubmitLogin")
+    private WebElement signInButton;
+
+    @FindBy(id = "email_create")
+    private WebElement emailSignUpField;
+
+    @FindBy(id = "SubmitCreate")
+    private WebElement signUpButton;
+
+    public void login(String email, String passwd){
+        emailField.sendKeys(email);
+        password.sendKeys(passwd);
+        signInButton.click();
+    }
+
+    public void signUp(String email){
+        emailSignUpField.sendKeys(email);
+        signUpButton.click();
+    }
+}
